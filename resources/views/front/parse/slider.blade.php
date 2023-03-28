30077666535d36d9e28a501e94d6dc5c5a10873c
Jacek <kontakt@jk-design.pl>
2023-03-28 18:26:46 +0200

    Galleries inside text

--- resources/views/front/parse/slider.blade.php
+++ resources/views/front/parse/slider.blade.php
@@ -4,9 +4,9 @@
             <li>
                 <a href="{{ asset('/uploads/gallery/images/'.$p->file) }}" class="swipebox" data-fslightbox="gallery-{{ $p->gallery_id }}" title="">
                 <picture>
-                    <source srcset="{{ asset('/uploads/gallery/images/webp/'.$p->file_webp) }}" type="image/webp">
-                    <source srcset="{{ asset('/uploads/gallery/images/'.$p->file) }}" type="image/jpeg">
-                    <img src="{{ asset('/uploads/gallery/images/'.$p->file) }}" alt="{{ $p->name }}" loading="lazy" width="676" height="507" class="w-100">
+                    <source srcset="{{ asset('/uploads/gallery/images/thumbs/webp/'.$p->file_webp) }}" type="image/webp">
+                    <source srcset="{{ asset('/uploads/gallery/images/thumbs/'.$p->file) }}" type="image/jpeg">
+                    <img src="{{ asset('/uploads/gallery/images/thumbs/'.$p->file) }}" alt="{{ $p->name }}" loading="lazy" width="676" height="507" class="w-100">
                 </picture>
                 </a>
             </li>
