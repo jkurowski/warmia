f22a8ef41b60c3e7454c70a42c84fd2008a9c516
Jacek <kontakt@jk-design.pl>
2023-03-29 09:45:15 +0200

    Homepage - contact

--- app/Http/Controllers/Front/IndexController.php
+++ app/Http/Controllers/Front/IndexController.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Box;
 use App\Models\Gallery;
 use App\Models\Inline;
+use App\Models\Page;
 use App\Models\RodoRules;
 
 class IndexController extends Controller
@@ -21,6 +22,7 @@ class IndexController extends Controller
             'boxes' => $boxes,
             'galleries' => $galleries,
             'array' => Inline::getElements(1),
+            'contact' => Page::find(2),
             'rules' => RodoRules::orderBy('sort')->whereActive(1)->get()
         ]);
     }
